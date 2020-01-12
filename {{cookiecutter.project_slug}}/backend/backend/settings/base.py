d85a158389811df967ac27b88e27701fae3b79ac
Alvaro Duran <alvaro.duranb@gmail.com>
2020-01-12 07:37:42 +0100

    separate settings into dev and prod

--- {{cookiecutter.project_slug}}/backend/backend/settings/base.py
+++ {{cookiecutter.project_slug}}/backend/backend/settings/base.py
@@ -13,21 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')w4vem*wxv2y(qp+*p)m(1ko496-)4473w08!=-hf2vkuq5tbr'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = [
-    "backend",
-]
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 
 
 # Application definition
---
+++ {{cookiecutter.project_slug}}/backend/backend/settings/development.py
@@ -0,0 +1,13 @@
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ')w4vem*wxv2y(qp+*p)m(1ko496-)4473w08!=-hf2vkuq5tbr'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    "backend",
+]
