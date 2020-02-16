0baffee7779df05cfafa4ce358ed0da5a833287d
ohduran <alvaro.duranb@hotmail.com>
2020-02-16 16:25:17 +0100

    TS-1: Expose port 3000 as 80 in local environment

--- {{cookiecutter.project_slug}}/backend/contrib/serializers.py
+++ {{cookiecutter.project_slug}}/backend/contrib/serializers.py
@@ -1,8 +1,3 @@
 from rest_framework import serializers
 
-
-class CharCountSerializer(serializers.Serializer):
-    count = serializers.SerializerMethodField()
-
-    def get_count(self, obj):
-        return obj.count
+# Create your serializers here.
