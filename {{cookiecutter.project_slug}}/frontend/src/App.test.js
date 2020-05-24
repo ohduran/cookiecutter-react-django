b8f1087813ce9e98b5d5c8443a6c439fbf425dba
ohduran <alvaro.duranb@hotmail.com>
2020-05-24 19:57:27 +0200

    replace waitforElement with waitFor and node version in production

--- {{cookiecutter.project_slug}}/frontend/src/App.test.js
+++ {{cookiecutter.project_slug}}/frontend/src/App.test.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import { render, fireEvent, waitForElement } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import App from './App';
-import axiosMock from 'axios';
+import React from "react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
+import axiosMock from "axios";
 
+jest.mock("axios");
 
-jest.mock('axios');
-
-test('on calling char_count it renders the number of characters', async () => {
+test("on calling char_count it renders the number of characters", async () => {
   const testString = "React Testing";
   const url = `/char_count?text=${testString}`;
   axiosMock.get.mockResolvedValueOnce({
     data: { count: testString.length },
-  })
+  });
   const { getByText, getByPlaceholderText, getByTestId } = render(<App />);
 
   const charInputElement = getByPlaceholderText("my string");
   expect(charInputElement).toBeInTheDocument();
-  
+
   fireEvent.change(charInputElement, { target: { value: testString } });
-  fireEvent.click(getByText('have?'));
-  const responseTextNode = await waitForElement(() => getByTestId('char-count'));
-  
-  expect(axiosMock.get).toHaveBeenCalledTimes(1)
-  expect(axiosMock.get).toHaveBeenCalledWith(url)
-  expect(getByTestId('char-count')).toHaveTextContent(`${testString.length} characters!`)
+  fireEvent.click(getByText("have?"));
+  const responseTextNode = await waitFor(() => getByTestId("char-count"));
+
+  expect(axiosMock.get).toHaveBeenCalledTimes(1);
+  expect(axiosMock.get).toHaveBeenCalledWith(url);
+  expect(getByTestId("char-count")).toHaveTextContent(
+    `${testString.length} characters!`
+  );
 });
